66846ce5adbf8c93f87f9f15c66e1dc868f08070
Lucas Souza <lucas.rsouza@yduqs.com.br>
2026-03-02 18:01:35 -0300

    adicionando banco de dados sqlite3

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,7 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require
-// here.
+import "github.com/devfullcycle/13-GraphQL/internal/database"
 
-type Resolver struct{}
+type Resolver struct {
+	CategoryDb *database.Category
+}
